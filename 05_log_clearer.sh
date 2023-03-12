1c5c924707019a4b40eb165b756060e37937d64d
user <robert.gregor11@gmail.com>
2023-03-12 22:18:58 +0000

    05v1

---
+++ 05_log_clearer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script: 05 - Clearing Logs
+# Author: Robert Gregor
+# Date of latest revision: 121830FMAR23
+
+# Objectives - Write a log clearing bash script
+    # Write a bash script that performs the following tasks:
+        # Print to screen file size of log files before compression
+        # Compress contents of log files listed below to backup directory
+            # /var/log/syslog
+            # /var/log/wtmp
+        # File name should contain time stamp with following format -YYYYMMDDHHMMSS
+            # Example: /var/log/backups/syslog-20220928081457.zip
+        # Clear contents of log file
+        # Print to screen file size of compressed file
+        # Compare size of compressed files to size of original log files
+
+# Stretch Goals - How can your script be used to clear tracks?
+    # Watch [How To Clear Tracks & Logs On Linux](https://www.youtube.com/watch?v=TgquV_OA-lU)
+        # Using knowledge of various logs described in video, expand script; what other system logs can you clear?
+    # In comments per log cleared, explain what the log tracks in the Ubuntu system. Why is this log important?
+
+# Code Fellows Sources:
+    # [How To Clear Tracks & Logs On Linux](https://www.youtube.com/watch?v=TgquV_OA-lU)
+
+# My Sources:
+    # 
+
+# Main
+
+
+
+# End
