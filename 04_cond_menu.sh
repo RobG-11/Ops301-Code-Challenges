5b13aced2dd3567b6414a2708b43bdbbb2a9139b
user <robert.gregor11@gmail.com>
2023-03-13 17:27:44 +0000

    05v2

--- 04_cond_menu.sh
+++ 04_cond_menu.sh
@@ -92,6 +92,8 @@ do
         hello_world
     elif [[ $option == "2" ]]; then
         ping_loopback
+    elif [[ $option == "3" ]]; then
+        net_adap_info
     else
         echo "Invalid Input!"
     fi
