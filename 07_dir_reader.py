a2f47e7e705727e6de8abe3c1ac31427bc9e6c9e
user <robert.gregor11@gmail.com>
2023-03-18 15:10:24 +0000

    07final

--- 07_dir_reader.py
+++ 07_dir_reader.py
@@ -8,14 +8,6 @@
         # Script must use the os.walk() function from the os library
         # Script must enclose the os.walk() function within a python function that hands it the user input file path
 
-# Stretch Goals
-    # Have the script save the output as a .txt file
-        # Have the script open the .txt file with Libre Office Writer
-    # Add a function to your Python script that performs the following as a first step to prepare a test directory
-        # Takes a user input string
-        # Creates a directory named the string using os.makdirs function
-        # Create sub-directories within the directory named ‘string_01’, ‘string_02’, and ‘string_03’
-
 # My Sources:
     # [Python 3 – input() function](https://www.geeksforgeeks.org/python-3-input-function/)
     # [os.walk() in Python](https://www.geeksforgeeks.org/os-walk-python/)
@@ -38,12 +30,7 @@ click.clear()
 # Declares user_input function:
     # Function prints question, accepts user input, and then returns user_input
 
-def user_input():
-    print("Please enter name of directory path to view and press ENTER or type exit to exit dir_reader.py:")
-    file_path = input()
-    return file_path
-
-file_path = user_input()
+file_path = input("Please enter name of directory path to view and press ENTER or type exit to exit dir_reader.py:\n")
 
 for (root, dirs, files) in os.walk(file_path):
     ### Add a print command here to print ==root==
---
+++ 08_py_list_mod.py
@@ -0,0 +1,19 @@
+# Script: 07 - Directory Creation
+# Author: Robert Gregor
+# Date of latest revision: 152030FMAR23
+
+# Objectives
+    # Create a Python script that includes the following:
+        # Assign to a variable a list of ten string elements
+        # Print the fourth element of the list
+        # Print the sixth through tenth element of the list
+        # Change the value of the seventh element to “onion”
+
+# My Sources:
+    # [](https://www.w3schools.com/python/python_user_input.asp)
+
+#!/usr/bin/env python3
+
+# Import libraries
+
+user_list = input()
