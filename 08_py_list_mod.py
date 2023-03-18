1b355e17b13d121f7ee707a94c227bcbb8a1ee16
user <robert.gregor11@gmail.com>
2023-03-18 16:33:23 +0000

    08final

--- 08_py_list_mod.py
+++ 08_py_list_mod.py
@@ -10,10 +10,84 @@
         # Change the value of the seventh element to “onion”
 
 # My Sources:
-    # [](https://www.w3schools.com/python/python_user_input.asp)
+    # [Python Tutorial](https://www.w3schools.com/python/default.asp)
+    # [Python | Get a list as input from user](https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/)
+    # [Python break, continue and pass Statements](https://www.tutorialspoint.com/python/python_loop_control.htm)
 
 #!/usr/bin/env python3
 
+# Main
+
 # Import libraries
 
-user_list = input()
+import click
+
+# Functions
+
+def print_item():
+    click.clear()
+    print(user_list)
+    user_item = int(input("\nPlease enter number of item (left to right) to print: "))
+    print()
+    print(user_list[user_item-1])
+
+def print_item_range():
+    click.clear()
+    print(user_list)
+    user_item_1 = int(input("\nPlease enter number of first item in range (left to right) to print: "))
+    user_item_2 = int(input("Please enter number of last item in range (left to right) to print: "))
+    print()
+    print(user_list[user_item_1-1:user_item_2])
+
+def change_item():
+    click.clear()
+    print(user_list)
+    user_item = int(input("\nPlease enter number of item (left to right) whose string value you would like to change: "))
+    item_value = str(input("Please enter the new string value: "))
+    user_list[user_item-1] = item_value
+    print("\nBelow is your modified list:\n")
+    print(user_list)
+
+while True:
+
+    click.clear()
+    user_list = []
+
+    print("\n-------------------------------------------------------------------------")
+    print("Please enter ten strings for manipulation or exit to exit py_list_mod.py:")
+    print("-------------------------------------------------------------------------")
+
+    for i in range (0, 10):
+        list_item = str(input("Input String and press ENTER: "))
+        if list_item == "exit":
+            print("\npy_list_mod.py exited successfully!\n")
+            exit()
+        user_list.append(list_item)
+
+    click.clear()
+    print("\n------------------------")
+    print("Your list is shown below")
+    print("------------------------")
+    print(user_list)
+
+    while True:
+              
+        print("\n------------------------")
+        print("Please choose an option:\n1) Print one item\n2) Print range of items\n3) Change item\n4) Input new list\nexit) exit program")
+        user_mod = str(input("------------------------\n"))
+
+        if user_mod == "1":
+            print_item()
+        elif user_mod == "2":
+            print_item_range()
+        elif user_mod == "3":
+            change_item()
+        elif user_mod == "4":
+            break
+        elif user_mod == "exit":
+            print("\npy_list_mod.py exited successfully!\n")
+            exit()
+        else:
+            print("\nInvalid input please try again!")
+            continue
+# End
