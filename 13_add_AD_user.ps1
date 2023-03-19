ca5537a4cffc4ae4a5e9c27a69098f0bc35aa692
user <robert.gregor11@gmail.com>
2023-03-19 15:45:25 +0000

    13v1

---
+++ 13_add_AD_user.ps1
@@ -0,0 +1,19 @@
+# Script: 13 - Powershell AD Automation
+# Author: Robert Gregor
+# Date of latest revision: 192030FMAR23
+
+# Objectives
+    # Write a Powershell script that adds the below person to AD
+        # Franz Ferdinand is the TPS Reporting Lead at GlobeX USA in Springfield, OR office
+        # Franz is part of the TPS Department
+        # Franz’s email is ferdi@GlobeXpower.com
+    # Test your script. Verify in ADAC that the user was created with the correct attributes
+        # If anything is missing, delete the user in ADAC and re-attempt from Powershell ISE
+
+# Code Fellows Sources:
+    # [New-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-aduser?view=windowsserver2022-ps)
+
+# My Sources:
+    # [
+
+# Main
