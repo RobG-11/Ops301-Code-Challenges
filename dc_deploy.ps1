fb1d53c2fa453fdd1c0c116a7db6d86e6d08404b
user <robert.gregor11@gmail.com>
2023-04-01 02:30:11 +0000

    projv1

--- dc_deploy.ps1
+++ dc_deploy.ps1
@@ -13,8 +13,10 @@
         # Creates users
 
 # My Sources:
+    # [Press Any Key to Continue in PowerShell](https://java2blog.com/press-any-key-to-continue-powershell/)
     # [Rename-Computer](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-7.3)
     # [PowerTip: Use PowerShell to Get Computer Name](https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-get-computer-name/)
+    # [How to set static & DHCP IP addresses in PowerShell](https://www.pdq.com/blog/using-powershell-to-set-static-and-dhcp-ip-addresses-part-1/)
     # [Get-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/get-aduser?view=windowsserver2022-ps)
     # [Format-Table](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.3)
 
@@ -31,7 +33,7 @@ function New-Computer-Name{
     Write-Host ""
     Write-Host "Your current computer name is $env:computername"
     Write-Host ""
-    $new_comp_name = Read-Host "Please provide the new computer name and press enter"
+    $new_comp_name = Read-Host "Please provide the new computer name"
 
     Rename-Computer -NewName $new_comp_name
     Write-Host ""
@@ -41,7 +43,26 @@ function New-Computer-Name{
     exit
 }
 
-
+function Assign-IP-DNS{
+    clear
+    Write-Host ""
+    Write-Host "Welcome to static IPv4 and DNS configuration"
+    Write-Host "-------------------------------------"
+    $static_IP = Read-Host "Please enter the IPv4 static address"
+    Write-Host ""
+    $subnet_mask = Read-Host "Please enter the subnet mask"
+    Write-Host ""
+    $default_gateway = Read-Host "Please enter the default gateway"
+    Write-Host ""
+    $dns_server = Read-Host "Please enter the DNS server address"
+    Write-Host ""
+    Read-Host "Press any key to implement changes and verify"
+    Write-Host ""
+    ipconfig /all
+    Write-Host ""
+    Read-Host "Press any key to continue..."
+    exit
+}
 
 function Create-NewUser {
     clear
